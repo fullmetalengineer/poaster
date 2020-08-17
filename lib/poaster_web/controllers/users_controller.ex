cb65cb31614761f8fcf3417b953b253ec925aa3d
Alan Long <alan.d.long@outlook.com>
2020-08-17 10:50:14 -0500

    Automatically log the newly created user in

--- lib/poaster_web/controllers/users_controller.ex
+++ lib/poaster_web/controllers/users_controller.ex
@@ -11,6 +11,59 @@ defmodule PoasterWeb.UsersController do
       |> json(user_with_personas(user))
   end
 
+  def update(conn, %{"id" => id, "persona" => persona_params}) do
+    # persona = Accounts.get_persona!(id)
+    # result = Accounts.update_persona(persona, persona_params)
+
+    # if Accounts.user_owns_persona?(conn.assigns[:signed_user], persona) do
+    #   case result do
+    #     {:ok, persona} ->
+    #       conn
+    #         |> put_status(:created)
+    #         |> json(%{
+    #           success: true,
+    #           persona: persona_data(persona)
+    #         })
+
+    #     {:error, _changeset} ->
+    #       conn
+    #         |> put_status(:internal_server_error)
+    #         |> json(%{
+    #             success: false,
+    #             error: %{
+    #               detail: "There was an issue updating your persona. Please try again!"
+    #             }
+    #           })
+    #   end
+    # else
+    #   action_not_allowed(conn)
+    # end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    # Delete a persona
+    # TODO: Mark as deleted, not actual delete?
+    # user = conn.assigns[:signed_user]
+    # persona = Accounts.get_persona!(id)
+
+    # if Accounts.user_owns_persona?(user, persona) do
+    #   result = Accounts.delete_persona(persona)
+    #   case result do
+    #     {:ok, %Persona{}} ->
+    #       conn
+    #         |> put_status(:ok)
+    #         |> json(%{success: true})
+
+    #     {:error, _changeset} ->
+    #       conn
+    #         |> put_status(:internal_server_error)
+    #         |> json(%{success: false, error: %{ detail: "There was an issue deleting your persona. Please try again!"}})
+    #     end
+    # else
+    #   action_not_allowed(conn)
+    # end
+  end
+
   defp user_with_personas(user) do
     %{
       data: %{
