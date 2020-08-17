59a0c3c282d30e3994e6beea378f6bd46c652a62
Alan Long <alan.d.long@outlook.com>
2020-08-17 14:17:09 -0500

    Min and Max username limitations

--- lib/poaster/accounts/persona.ex
+++ lib/poaster/accounts/persona.ex
@@ -19,8 +19,28 @@ defmodule Poaster.Accounts.Persona do
     persona
     |> cast(attrs, [:username, :name, :bio, :profile_image_url, :background_image_url, :user_id])
     |> validate_required([:username])
+    |> validate_min_length(:username, 2)
+    |> validate_max_length(:username, 20)
     |> unique_constraint(:username, name: :personas_username_index)
   end
 
+  def validate_min_length(changeset, field, text_length) do
+    validate_change(changeset, field, fn (current_field, value) ->
+      if String.length(value) < text_length do
+        [current_field: "must be at least #{text_length} characters"]
+      else
+        []
+      end
+    end)
+  end
 
+  def validate_max_length(changeset, field, text_length) do
+    validate_change(changeset, field, fn (current_field, value) ->
+      if String.length(value) > text_length do
+        [current_field: "must be no longer than #{text_length} characters"]
+      else
+        []
+      end
+    end)
+  end
 end
