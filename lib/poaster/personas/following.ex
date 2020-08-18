681f5118a79cb4a9fc3a767b6585544c35a15a84
Alan Long <alan.d.long@outlook.com>
2020-08-18 16:05:50 -0500

    Add constraints, validations to followings model

--- lib/poaster/personas/following.ex
+++ lib/poaster/personas/following.ex
@@ -13,7 +13,8 @@ defmodule Poaster.Personas.Following do
   @doc false
   def changeset(following, attrs) do
     following
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:persona_id, :followed_id])
+    |> validate_required([:persona_id, :followed_id])
+    |> unique_constraint([:persona_id, :followed_id], name: :followings_persona_id_followed_id_index)
   end
 end
